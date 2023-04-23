29c68ed3f2997d65febca5ea63e430f5dba9dbdb
jomanaaljarallah <jarallahjomana@gmal.com>
2023-04-23 06:16:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+mondayWork();
+function wrapAdjective(st='*'){
+    return  function (set='special'){
+        return `You are ${st}${set}${st}!`;
+    }
+}
+     
+    
+      wrapAdjective("||")("a dedicated programmer");
